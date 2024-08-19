f8997307e9dca6059fbf840085becc06a5649c48
Nhan Dong <nhandong0205@gmail.com>
2024-08-19 23:46:43 +0700

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,42 @@
+import { Link } from 'react-router-dom';
+import AmazonLogo from '../images/amazon-logo-white.png'
+import AmazonMobile from '../images/amazon-mobile-logo-white.png'
+import SearchIcon from '../images/icons/search-icon.png'
+import CartIcon from '../images/icons/cart-icon.png'
+
 import '../styles/Header.css';
 
 export default function Header() {
     return (
       <div className="amazon-header">
         <div className="amazon-header-left-section">
-          <a href="home" className="header-link">
+          <Link href="home" className="header-link">
             <img className="amazon-logo"
-              src="../images/amazon-logo-white.png" />
+              src={AmazonLogo} />
             <img className="amazon-mobile-logo"
-              src="../images/amazon-mobile-logo-white.png" />
-          </a>
+              src={AmazonMobile} />
+          </Link>
         </div>
 
         <div className="amazon-header-middle-section">
           <input className="search-bar" type="text" placeholder="Search" />
 
           <button className="search-button">
-            <img className="search-icon" src="../images/icons/search-icon.png" />
+            <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
 
         <div className="amazon-header-right-section">
-          <a className="orders-link header-link" href="orders">
+          <Link className="orders-link header-link" href="orders">
             <span className="returns-text">Returns</span>
             <span className="orders-text">& Orders</span>
-          </a>
+          </Link>
 
-          <a className="cart-link header-link" href="checkout">
-            <img className="cart-icon" src="../images/icons/cart-icon.png" />
+          <Link className="cart-link header-link" href="checkout">
+            <img className="cart-icon" src={CartIcon} />
             <div className="cart-quantity"></div>
             <div className="cart-text">Cart</div>
-          </a>
+          </Link>
         </div>
       </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "checkout",
+        element: <App.Checkout />,
+      },
+      {
+        path: "orders",
+        element: <App.Orders />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
