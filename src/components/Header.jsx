f6ce760ab1b7b4a5e80467a7c3fabc9c7b6fa281
Nhan Dong <nhandong0205@gmail.com>
2024-08-19 20:40:20 +0700

    Fix images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,34 @@
 import '../styles/Header.css';
 
-export default function Header({score}) {
+export default function Header() {
     return (
       <div className="amazon-header">
         <div className="amazon-header-left-section">
-          <a href="amazon.html" className="header-link">
+          <a href="home" className="header-link">
             <img className="amazon-logo"
-              src="images/amazon-logo-white.png" />
+              src="../images/amazon-logo-white.png" />
             <img className="amazon-mobile-logo"
-              src="images/amazon-mobile-logo-white.png" />
+              src="../images/amazon-mobile-logo-white.png" />
           </a>
         </div>
 
         <div className="amazon-header-middle-section">
-          <input className="search-bar js-search-bar" type="text" placeholder="Search" />
+          <input className="search-bar" type="text" placeholder="Search" />
 
-          <button className="search-button js-search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
+          <button className="search-button">
+            <img className="search-icon" src="../images/icons/search-icon.png" />
           </button>
         </div>
 
         <div className="amazon-header-right-section">
-          <a className="orders-link header-link" href="orders.html">
+          <a className="orders-link header-link" href="orders">
             <span className="returns-text">Returns</span>
             <span className="orders-text">& Orders</span>
           </a>
 
-          <a className="cart-link header-link" href="checkout.html">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity js-cart-quantity"></div>
+          <a className="cart-link header-link" href="checkout">
+            <img className="cart-icon" src="../images/icons/cart-icon.png" />
+            <div className="cart-quantity"></div>
             <div className="cart-text">Cart</div>
           </a>
         </div>
